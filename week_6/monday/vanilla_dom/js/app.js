5b79f32fca2f822728c541135b4dd6fea9f1ae7c
LadyG <ghgilliard@gmail.com>
2017-12-13 18:31:42 -0500

    saving changes

--- week_6/monday/vanilla_dom/js/app.js
+++ week_6/monday/vanilla_dom/js/app.js
@@ -1,6 +1,7 @@
-windows.onload = function () {
+window.onload = function () {
     const title = document.getElementById('page-title');
-    const awesomeList = document.getElementById("awesome-list");
+    const awesomeList = document.getElementById('awesome-list');
+    const awesomeListItems = document.getElementById('awesome-list-item');
     const newLi = document.createElement('li');
     newLi.classList.add('awesome-list-item');
     newLi.innerHTML = "everybody loves vanilla";
@@ -17,11 +18,12 @@ windows.onload = function () {
 
     const list = document.getElementsByClassName('awesome-list-item');
     for (let i=0; i<list.length; i++) {
+        let currentItem = list[i];
         currentItem.classList.add('blue');
     }
 
     const footer = document.createElement('footer');
     footer.innerHTML = "thanks for visting .... Have an Ice Day!"
     footer.classList.add('red');
-    document.body.appendChild(footer);
+    document.body.appendChild(footer);//placement of the element
 }
