a3db7afb0323cc607373fb15832ba19cecf0b6d9
wishaw <William.Shaw@mlb.com>
2017-12-13 20:12:42 -0500

    added trillo solution

--- week_6/wed/instructor/events_practice/app.js
+++ week_6/wed/instructor/events_practice/app.js
@@ -1,1 +1,23 @@
 console.log('loaded');
+// MONDAY NIGHT REVIEW
+
+document.getElementById('taco');
+document.getElementsByClassName('tacos');
+document.querySelector('.tacos');
+document.querySelectorAll('.tacos');
+
+document.createElement('li');
+
+window.onload = function () {
+    const h1 = document.querySelector('#random-div h1');
+    const tacosElem = document.getElementById('tacos');
+
+    h1.addEventListener('click', function (e) {
+        var h1 = e.target;
+        document.body.style.backgroundColor = 'red';
+    });
+
+    tacosElem.addEventListener('dblclick', function () {
+        document.body.style.backgroundColor = 'cornflowerblue';
+    })
+  }
--- week_6/wed/instructor/ex_trillo/starter/js/app.js
+++ week_6/wed/instructor/ex_trillo/starter/js/app.js
@@ -1,1 +1,66 @@
+window.onload = function () {
+    const removeButtons = document.querySelectorAll('.delete');
+    const finishButtons = document.querySelectorAll('.finish');
+    const addCardButton = document.querySelector('#new-card-button');
 
+    for(let i = 0; i < removeButtons.length; i++) {
+        addRemoveListener(removeButtons[i]);
+    }
+
+    for(let i = 0; i < finishButtons.length; i++) {
+        addFinishListener(finishButtons[i]);
+    }
+
+    addCardButton.addEventListener('click', addNewCard);
+}
+
+function addRemoveListener(node) {
+    node.addEventListener('click', function (e) {
+        this.parentNode.remove();
+    });
+}
+
+function addFinishListener(node) {
+    node.addEventListener('click', function(e) {
+        const parentNode = this.parentNode;
+        this.remove();
+        parentNode.remove();
+        parentNode.classList.remove('todo');
+        parentNode.classList.add('done');
+        document.querySelector('#done-column .card-list').appendChild(parentNode);
+    });
+}
+
+function createFinishButton() {
+    const finish = document.createElement('span');
+    finish.classList.add('finish');
+    finish.innerHTML = 'Finish'
+
+    addFinishListener(finish);
+
+    return finish;
+}
+
+function createDeleteButton() {
+    const deleteButton = document.createElement('span');
+    deleteButton.classList.add('delete');
+    deleteButton.innerHTML = 'X';
+
+    addRemoveListener(deleteButton);
+
+    return deleteButton;
+}
+
+function addNewCard() {
+    const valueInput = document.querySelector('#new-card-text');
+    const value = valueInput.value;
+    const newLi = document.createElement('li');
+    newLi.classList.add('card');
+    newLi.classList.add('todo');
+    newLi.innerHTML = value;
+    newLi.appendChild(createDeleteButton());
+    newLi.appendChild(createFinishButton());
+
+    valueInput.value = '';
+    document.querySelector('#todo-column .card-list').appendChild(newLi);
+}
