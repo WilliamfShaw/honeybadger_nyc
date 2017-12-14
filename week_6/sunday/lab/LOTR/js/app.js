2c733ea663489666f79a6247f50249777d5392ec
wishaw <William.Shaw@mlb.com>
2017-12-14 10:08:18 -0500

    openlab

---
+++ week_6/sunday/lab/LOTR/js/app.js
@@ -0,0 +1,155 @@
+// ==============================
+//       Characters and places
+// ==============================
+
+const hobbits = [
+  'Frodo Baggins',
+  'Samwise "Sam" Gamgee',
+  'Meriadoc "Merry" Brandybuck',
+  'Peregrin "Pippin" Took'
+];
+
+const buddies = [
+  'Gandalf the Grey',
+  'Legolas',
+  'Gimli',
+  'Strider',
+  'Boromir'
+];
+
+const baddies = [
+  'Sauron',
+  'Saruman',
+  'The Uruk-hai',
+  'Orcs'
+];
+
+const lands = [
+  'The Shire',
+  'Rivendell',
+  'Mordor'
+];
+
+// ====================================
+//           Chapters
+// ====================================
+
+function makeMiddleEarth() {
+  console.log('Trying to make middle earth.');
+  // create a section tag with an id of middle-earth
+  let middleEarth = document.createElement('section');
+  // console.log(middleEarth); 
+  middleEarth.setAttribute('id', 'middle-earth');
+  // add each land to the section as article tags-- try using a loop
+  document.body.appendChild(middleEarth);
+
+  for(let land of lands){
+    // console.log(land);
+    let place = document.createElement('article');
+    // console.log(place);
+    // assign the id of the corresponding article tag as the name of the land 
+    place.setAttribute('id', land);
+    // console.log(place);
+    // inside each article tag include an h1 with the name of the land
+    middleEarth.appendChild(place); 
+    let nameOfLand = document.createElement('h1');
+    // console.log(nameOfLand);
+    nameOfLand.innerHTML = land;
+    // console.log(nameOfLand);
+    place.appendChild(nameOfLand);
+  }
+  // append the section to the body of the DOM with: document.body.appendChild( // variable name )
+};
+
+function makeHobbits() {
+  console.log('Make hobbits');
+  // display an unordered list of hobbits in the shire
+  // give each hobbit a class of "hobbit"
+  // list the hobbits alphabetically.  Maybe use the .sort() method.
+};
+
+function keepItSecretKeepItSafe() {
+  // create an empty div with an id of 'the-ring' and innerHTML of "The ring"
+  // add the ring as a child of Frodo
+};
+
+function makeBaddies() {
+  // display an unordered list of baddies in Mordor
+  // give each of the baddies a class of "baddy"
+};
+
+function makeBuddies() {
+  // create an aside tag and append it below mordor
+  // display an unordered list of buddies in the aside
+  // give each of the buddies a class of "buddy"
+};
+
+function leaveTheShire() {
+  // grab the hobbits and move them to Rivendell
+};
+
+function beautifulStranger() {
+  // change the buddy 'Strider' node to "Aragorn"
+};
+
+function forgeTheFellowShip() {
+  // move the hobbits and the buddies to Rivendell
+  // create a new div called 'the-fellowship'
+  // add an h1 with the text 'The Fellowship' to this new div
+  // add each hobbit and buddy one at a time to 'the-fellowship'
+  // after each character is added make an alert that they have joined your party
+};
+
+function theBalrog() {
+  // change the inner HTML of the 'Gandalf' node to 'Gandalf the White'
+  // add a class "the-white" to this element
+  // in the style.css file, add a css rule for the class "the-white"
+  // have a white background and a grey border
+};
+
+function hornOfGondor() {
+  // pop up an alert that the horn of gondor has been blown
+  // Boromir's been killed by the Uruk-hai!
+  // put a linethrough on boromir's name
+  // Remove the Uruk-Hai from the Baddies on the page
+};
+
+function itsDangerousToGoAlone(){
+  // take Frodo and Sam out of the fellowship and move them to Mordor
+  // add a div with an id of 'mount-doom' to Mordor
+};
+
+function weWantsIt() {
+  // Create a div with an id of 'gollum' and add it to Mordor
+  // Remove the ring from Frodo and give it to Gollum
+  // Create a condition (Maybe an IF statement) that changes the font-color of the ring div to Red if 
+  // it is a child of the div with the id 'gollum'
+  // Move Gollum into Mount Doom 
+};
+
+function thereAndBackAgain() {
+  // remove Gollum and the Ring from the DOM
+  // remove all the baddies from the DOM
+  // Move all the hobbits back to the shire
+};
+
+// =================================
+// What to do when the page is ready
+// =================================
+
+window.onload = function() {
+  document.querySelector('#b1').addEventListener('click', makeMiddleEarth);
+  document.querySelector('#b2').addEventListener('click', makeHobbits);
+  document.querySelector('#b3').addEventListener('click', keepItSecretKeepItSafe);
+  document.querySelector('#b4').addEventListener('click', makeBaddies);
+  document.querySelector('#b5').addEventListener('click', makeBuddies);
+  document.querySelector('#b6').addEventListener('click', leaveTheShire);
+  document.querySelector('#b7').addEventListener('click', beautifulStranger);
+  document.querySelector('#b8').addEventListener('click', forgeTheFellowShip);
+  document.querySelector('#b9').addEventListener('click', theBalrog);
+  document.querySelector('#b10').addEventListener('click', hornOfGondor);
+  document.querySelector('#b11').addEventListener('click', itsDangerousToGoAlone);
+  document.querySelector('#b12').addEventListener('click', weWantsIt);
+  document.querySelector('#b13').addEventListener('click', thereAndBackAgain);
+
+};
---
+++ week_6/sunday/lab/LOTR/lord_of_the_rings_hw.md
@@ -0,0 +1,34 @@
+## Trip To MorDOMr
+
+#### Learning Objectives
+
+- Practice using 'Vanilla' JavaScript to manipulate the DOM
+
+We are going to take a trip from the Shire, through Rivendell, across Middle
+Earth, and into the heart of Mordor itself, Mount Doom. Pack up, because we're
+going on an adventure.
+
+### Step 1-13
+
+Observe the 13 chapters that have been included in the starter code (app.js). Each
+chapter has a button with a click handler in the html file, and an associated javascript function in app.js. Your job is to fill in the right code into the JS functions.
+
+Remember you can check your DOM tree in the Elements tab in Chrome Console.
+
+**Workflow**
+
+Tackle this problem one function from start to finish at a time.
+
+* Write a trivial console.log inside the function to check that the event
+  listener works when you click the right button
+* Write the correct DOM manipulatory code inside the function and check that it
+  works
+
+Each function depends on the previous ones, so make sure your function works
+before moving on to the next one!
+
+Every time you refresh the page, you will have to click the buttons one by one
+in order from the beginning to operate your application. (If you really don't want 
+the popups after you've completed a step, you can check to disable each popup and
+speed up the process of debugging).
+
