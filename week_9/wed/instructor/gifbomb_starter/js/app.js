1bf1481c1add9965a0fa847686db2a9d1d583f2c
wishaw <William.Shaw@mlb.com>
2018-01-04 09:54:15 -0500

    add gifbomb solution

--- week_9/wed/instructor/gifbomb_starter/js/app.js
+++ week_9/wed/instructor/gifbomb_starter/js/app.js
@@ -13,7 +13,15 @@ $(function() {
 // endpoint: http://api.giphy.com/v1/gifs/random
 // api_key: dc6zaTOxFJmzC&
 // Request will return an object with a single gif and metadata
-function getRandomGif() {};
+function getRandomGif() {
+	const result = $.ajax({
+		url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC'
+	})
+
+	result.then(function(data) {
+		renderResults([data.data]);
+	})
+};
 
 // Kitten Bomb
 // AND
@@ -21,13 +29,54 @@ function getRandomGif() {};
 // Performs an AJAX request to the giphy api
 // should ask for 10 gifs
 // should render and display them using the gifTemplate
-function kittenBomb() {};
-function puppyBomb() {};
+function kittenBomb() {
+	const result = $.ajax({
+		url: 'http://api.giphy.com/v1/gifs/search?q=kittens&limit=10&api_key=dc6zaTOxFJmzC'
+	})
+
+	result.then(function(data) {
+		renderResults(data.data);
+	})
+};
+
+function puppyBomb() {
+	const result = $.ajax({
+		url: 'http://api.giphy.com/v1/gifs/search?q=puppies&limit=10&api_key=dc6zaTOxFJmzC'
+	})
+
+	result.then(function(data) {
+		renderResults(data.data);
+	})
+};
 
 // Performs an AJAX request to the giphy api
 // with user supplied search params
-function search() {};
+function search() {
+	const value = $('#search-box input').val();
+	const result = $.ajax({
+		url: `http://api.giphy.com/v1/gifs/search?q=${value}&limit=10&api_key=dc6zaTOxFJmzC`
+	})
+
+	result.then(function(data) {
+		renderResults(data.data);
+	})
+};
 
 // Should render and display the results
 // returned from the AJAX request to giphy
-function renderResults(results) {};
+function renderResults(results) {
+	console.log(results);
+	const $gifContainer = $('#gif-container');
+	$gifContainer.empty();
+
+	results.forEach(function(result) {
+		const image = result.image_original_url || result.images.original.url;
+		const markup = `
+				<div class='gif'>
+				<img src="${image}" alt="">
+			</div>
+		`;
+
+		$gifContainer.append(markup);
+	})
+};
