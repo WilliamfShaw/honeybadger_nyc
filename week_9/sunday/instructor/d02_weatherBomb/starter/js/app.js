2723ca63480644d146b665fb26b852f8d6177e55
wishaw <William.Shaw@mlb.com>
2018-01-07 12:44:39 -0500

    weather bomb solution

---
+++ week_9/sunday/instructor/d02_weatherBomb/starter/js/app.js
@@ -0,0 +1,56 @@
+$(function() {
+    function getWeatherData(city) {
+        const result = $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/weather',
+            data: {
+                q: city,
+                APPID: apiKey,
+                units: 'imperial'
+            },
+            dataType: 'json',
+            method: 'GET'
+        });
+
+        return result;
+    }
+
+    function transformWeatherData(weatherData) {
+        const cityName = weatherData.name;
+        const temperature = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const lat = weatherData.coord.lat;
+        const long = weatherData.coord.lon;
+
+        // const { name, main: { temp: temperature }, weather: [ { description } ], coord: { lat, lon: long} } = weatherData;
+
+        return {
+            cityName,
+            temperature,
+            description,
+            lat,
+            long
+        }
+    }
+
+    function renderWeatherData(updatedWeatherData) {
+        //LOAD THE DATA
+        const { cityName, temperature, description, lat, long } = updatedWeatherData;
+        const template = `
+            <h1>${cityName}</h1>
+            <h2>${temperature}</h2>
+            <p>${description}</p>
+            <span>LATITUDE: ${lat} || LONGTITUDE: ${long}</span>
+        `;
+
+        $('#results').append(template);
+    }
+
+    getWeatherData('New York')
+        .then(transformWeatherData)
+        .then(renderWeatherData)
+
+    getWeatherData('London')
+        .then(transformWeatherData)
+        .then(renderWeatherData)
+    ;
+})
